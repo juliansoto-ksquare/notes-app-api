92d8d6f84fc48f4c470a017b66d0d4a4ab459389
juliansoto <juliansotoponce@gmail.com>
2019-11-13 10:29:43 -0600

    add API CRUD methods

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cuid = require('cuid');
+
+const notes = [];
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
     res.status(204).end();
 })
+
+app.post('/notes', (req, res, next) => {
+    const {title, content} = req.body;
+
+    const note = {
+        title,
+        content,
+        id: cuid()
+    };
+
+    notes.push(note);
+
+    res.status(201).json({note});
+});
+
+app.get('/notes', (req, res, next) => {
+    res.json({notes});
+})
+
+app.put('/notes/:id', (req, res, next) => {
+    const {id} = req.params;
+    const {title, content} = req.body;
+    const index = notes.map(note => note.id).indexOf(id);
+
+    if (index === -1) res.status(404).json({});
+
+    const note = notes[index];
+
+    note.title = title;
+    note.content = content;
+
+    res.json({note});
+});
+
+app.delete('/notes/:id', (req, res, next) => {
+    const {id} = req.params;
+    const index = notes.map(note => note.id).indexOf(id);
+
+    if (index === -1) res.status(404).json({});
+    
+    notes.splice(index, 1);
+
+    res.status(200).json({});
+});
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,11 @@ const supertest = require('supertest');
 const {expect} = require('chai');
 const app = require('../app');
 
+const testNote = {
+    title: 'asdf',
+    content: 'qwerty'
+};
+
 it('should return no content from root', () => {
     supertest(app)
     .get('/')
@@ -9,4 +14,73 @@ it('should return no content from root', () => {
     .end((err, res) => {
         if (err) throw err;
     });
+});
+
+it('should create a new note with POST', () => {
+    supertest(app)
+    .post('/notes')
+    .send(testNote)
+    .expect('Content-Type', /json/)
+    .expect(201)
+    .end((err, res) => {
+        if (err) throw err;
+
+        expect(res.body.note.id).to.be.ok;
+        testNote.id = res.body.note.id;
+        expect(res.body.note.title).to.be.equal(testNote.title);
+        expect(res.body.note.content).to.be.equal(testNote.content);
+    })
+});
+
+it('should GET the list of notes', () => {
+    supertest(app)
+    .get('/notes')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+        if (err) throw err;
+
+        expect(res.body.notes).to.be.deep.equal([testNote]);
+    })
+});
+
+it('should edit a note with PUT', () => {
+    supertest(app)
+    .put(`/notes/${testNote.id}`)
+    .send({
+        title: 'new title',
+        content: 'new content'
+    })
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+        if (err) throw err;
+
+        expect(res.body.note.title).to.be.equal('new title');
+        expect(res.body.note.content).to.be.equal('new content');
+        expect(res.body.note.id).to.be.ok;
+    })
+});
+
+it('should DELETE a note', () => {
+    supertest(app)
+    .delete(`/notes/${testNote.id}`)
+    .expect(200)
+    .end((err, res) => {
+        if (err) throw err;
+    
+        expect(res.body).to.be.deep.equal({});
+    })
+});
+
+it('should GET an empty array of notes', () => {
+    supertest(app)
+    .get('/notes')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+        if (err) throw err;
+
+        expect(res.body.notes).to.be.deep.equal([]);
+    })
 })
